7f169557583797ad8154bf09d9c59aba6f5fd9c8
xluss4x <lussa.decarvalho@gmail.com>
2024-09-05 20:38:43 +0100

    i started making the backend

---
+++ source/index.js
@@ -0,0 +1,8 @@
+let nameInput = document.getElementById('user-input');
+let fontStyle = document.getElementById('font-style');
+let fontSize = document.getElementById('font-size');
+let generateButton = document.getElementById('color-button');
+
+function selectColor() {
+    
+}
