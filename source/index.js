820c3b411b2cefa3504456cd2aabcc0ab4ecd15b
xluss4x <lussa.decarvalho@gmail.com>
2024-09-10 16:24:16 +0100

    i made the text generation functionality

--- source/index.js
+++ source/index.js
@@ -1,8 +1,23 @@
-let nameInput = document.getElementById('user-input');
-let fontStyle = document.getElementById('font-style');
-let fontSize = document.getElementById('font-size');
-let generateButton = document.getElementById('color-button');
+const userInput = document.getElementById("user-input");
+const fontStyle = document.getElementById("font-style");
+const colorSelector = document.getElementById("color-selector");
+const fontSize = document.getElementById("font-size");
+const neonText = document.querySelector(".neonText");
+const canvas = document.querySelector(".image");
 
-function selectColor() {
-    
+function generateText() {
+    const text = userInput.value;
+    const font = fontStyle.value;
+    const color = colorSelector.value;
+    const size = fontSize.value;
+    neonText.textContent = text;
+    neonText.style.fontFamily = font;
+    neonText.style.color = color;
+    neonText.style.fontSize = `${size}px`;
 }
+function generateImages() {
+    const ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(neonText, 0, 0);
+}
+
